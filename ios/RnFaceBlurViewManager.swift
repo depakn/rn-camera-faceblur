999342e801dac9a0d18bb1adce5582a43b5d0b2c
Deepak Natarajan <deepak@local>
2024-08-11 16:26:39 +0530

    feat: working with bugs

--- ios/RnFaceBlurViewManager.swift
+++ ios/RnFaceBlurViewManager.swift
@@ -1,49 +1,49 @@
-import UIKit
 import AVFoundation
-import Vision
 import CoreImage
 import Photos
+import UIKit
+import Vision
 
 @objc(RnFaceBlurViewManager)
 class RnFaceBlurViewManager: RCTViewManager {
 
-    override func view() -> UIView! {
-        return RnFaceBlurView()
-    }
+  override func view() -> UIView! {
+    return RnFaceBlurView()
+  }
 
-    override static func requiresMainQueueSetup() -> Bool {
-        return true
-    }
+  override static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
 
-    @objc func startCamera(_ node: NSNumber) {
-        self.bridge.uiManager.addUIBlock { (uiManager, viewRegistry) in
-            if let view = viewRegistry?[node] as? RnFaceBlurView {
-                DispatchQueue.main.async {
-                    view.startCamera()
-                }
-            }
+  @objc func startCamera(_ node: NSNumber) {
+    self.bridge.uiManager.addUIBlock { (uiManager, viewRegistry) in
+      if let view = viewRegistry?[node] as? RnFaceBlurView {
+        DispatchQueue.main.async {
+          view.startCamera()
         }
+      }
     }
+  }
 
-    @objc func stopCamera(_ node: NSNumber) {
-        self.bridge.uiManager.addUIBlock { (uiManager, viewRegistry) in
-            if let view = viewRegistry?[node] as? RnFaceBlurView {
-                DispatchQueue.main.async {
-                    view.stopCamera()
-                }
-            }
+  @objc func stopCamera(_ node: NSNumber) {
+    self.bridge.uiManager.addUIBlock { (uiManager, viewRegistry) in
+      if let view = viewRegistry?[node] as? RnFaceBlurView {
+        DispatchQueue.main.async {
+          view.stopCamera()
         }
+      }
     }
+  }
 
-    @objc func flipCamera(_ node: NSNumber) {
-        self.bridge.uiManager.addUIBlock { (uiManager, viewRegistry) in
-            if let view = viewRegistry?[node] as? RnFaceBlurView {
-                DispatchQueue.main.async {
-                    view.flipCamera()
-                }
-            }
+  @objc func flipCamera(_ node: NSNumber) {
+    self.bridge.uiManager.addUIBlock { (uiManager, viewRegistry) in
+      if let view = viewRegistry?[node] as? RnFaceBlurView {
+        DispatchQueue.main.async {
+          view.flipCamera()
         }
+      }
     }
+  }
 }
 
 class RnFaceBlurView: UIView {
@@ -53,6 +53,13 @@ class RnFaceBlurView: UIView {
   private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
   private var movieOutput = AVCaptureMovieFileOutput()
   private var currentCameraPosition: AVCaptureDevice.Position = .front
+
+  private var assetWriter: AVAssetWriter?
+  private var assetWriterInput: AVAssetWriterInput?
+  private var pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor?
+
+  private var videoOutputURL: URL?
+  private var startTime: CMTime?
 
   // Save Variables
   private var isRecording = false
@@ -69,16 +76,16 @@ class RnFaceBlurView: UIView {
 
   @objc func startCamera() {
     if !captureSession.isRunning {
-        captureSession.startRunning()
+      captureSession.startRunning()
     }
     startRecording()
   }
 
   @objc func stopCamera() {
     if captureSession.isRunning {
-        stopRecording()
-        captureSession.stopRunning()
-        clearDrawings()
+      stopRecording()
+      captureSession.stopRunning()
+      clearDrawings()
     }
   }
 
@@ -86,24 +93,28 @@ class RnFaceBlurView: UIView {
     captureSession.beginConfiguration()
 
     guard let currentInput = captureSession.inputs.first as? AVCaptureDeviceInput else {
-        captureSession.commitConfiguration()
-        return
+      captureSession.commitConfiguration()
+      return
     }
 
     captureSession.removeInput(currentInput)
 
     currentCameraPosition = currentCameraPosition == .front ? .back : .front
 
-    guard let newDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: currentCameraPosition).devices.first else {
-        fatalError("No camera available")
+    guard
+      let newDevice = AVCaptureDevice.DiscoverySession(
+        deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: currentCameraPosition
+      ).devices.first
+    else {
+      fatalError("No camera available")
     }
 
     let newInput = try! AVCaptureDeviceInput(device: newDevice)
 
     if captureSession.canAddInput(newInput) {
-        captureSession.addInput(newInput)
+      captureSession.addInput(newInput)
     } else {
-        captureSession.addInput(currentInput)
+      captureSession.addInput(currentInput)
     }
 
     captureSession.commitConfiguration()
@@ -121,7 +132,12 @@ class RnFaceBlurView: UIView {
   }
 
   private func addCameraInput() {
-    guard let device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInTrueDepthCamera, .builtInDualCamera, .builtInWideAngleCamera], mediaType: .video, position: .front).devices.first else {
+    guard
+      let device = AVCaptureDevice.DiscoverySession(
+        deviceTypes: [.builtInTrueDepthCamera, .builtInDualCamera, .builtInWideAngleCamera],
+        mediaType: .video, position: .front
+      ).devices.first
+    else {
       fatalError("No camera detected. Please use a real camera, not a simulator.")
     }
 
@@ -136,10 +152,13 @@ class RnFaceBlurView: UIView {
   }
 
   private func getCameraFrames() {
-    videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString): NSNumber(value: kCVPixelFormatType_32BGRA)] as [String: Any]
+    videoDataOutput.videoSettings =
+      [(kCVPixelBufferPixelFormatTypeKey as NSString): NSNumber(value: kCVPixelFormatType_32BGRA)]
+      as [String: Any]
 
     videoDataOutput.alwaysDiscardsLateVideoFrames = true
-    videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+    videoDataOutput.setSampleBufferDelegate(
+      self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
 
     captureSession.addOutput(videoDataOutput)
 
@@ -147,7 +166,9 @@ class RnFaceBlurView: UIView {
       captureSession.addOutput(movieOutput)
     }
 
-    guard let connection = videoDataOutput.connection(with: .video), connection.isVideoOrientationSupported else {
+    guard let connection = videoDataOutput.connection(with: .video),
+      connection.isVideoOrientationSupported
+    else {
       return
     }
 
@@ -159,15 +180,53 @@ class RnFaceBlurView: UIView {
       isRecording = true
       let outputFileName = NSUUID().uuidString
       let outputFilePath = NSTemporaryDirectory() + "\(outputFileName).mov"
-      let outputURL = URL(fileURLWithPath: outputFilePath)
-      movieOutput.startRecording(to: outputURL, recordingDelegate: self)
+      videoOutputURL = URL(fileURLWithPath: outputFilePath)
+
+      guard let videoOutputURL = videoOutputURL else { return }
+
+      do {
+        assetWriter = try AVAssetWriter(outputURL: videoOutputURL, fileType: .mov)
+
+        let videoSettings: [String: Any] = [
+          AVVideoCodecKey: AVVideoCodecType.h264,
+          AVVideoWidthKey: NSNumber(value: Float(self.bounds.width)),
+          AVVideoHeightKey: NSNumber(value: Float(self.bounds.height)),
+        ]
+
+        assetWriterInput = AVAssetWriterInput(mediaType: .video, outputSettings: videoSettings)
+        assetWriterInput?.expectsMediaDataInRealTime = true
+
+        let sourcePixelBufferAttributes: [String: Any] = [
+          kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: kCVPixelFormatType_32ARGB),
+          kCVPixelBufferWidthKey as String: NSNumber(value: Float(self.bounds.width)),
+          kCVPixelBufferHeightKey as String: NSNumber(value: Float(self.bounds.height)),
+        ]
+
+        pixelBufferAdaptor = AVAssetWriterInputPixelBufferAdaptor(
+          assetWriterInput: assetWriterInput!,
+          sourcePixelBufferAttributes: sourcePixelBufferAttributes)
+
+        if assetWriter!.canAdd(assetWriterInput!) {
+          assetWriter!.add(assetWriterInput!)
+        }
+
+        assetWriter!.startWriting()
+        startTime = nil
+      } catch {
+        print("Error setting up asset writer: \(error)")
+        return
+      }
     }
   }
 
   private func stopRecording() {
     if isRecording {
-      movieOutput.stopRecording()
       isRecording = false
+      assetWriterInput?.markAsFinished()
+      assetWriter?.finishWriting { [weak self] in
+        guard let self = self, let videoOutputURL = self.videoOutputURL else { return }
+        self.saveVideoToPhotos(outputFileURL: videoOutputURL)
+      }
     }
   }
 
@@ -182,7 +241,8 @@ class RnFaceBlurView: UIView {
       }
     }
 
-    let imageResultHandler = VNImageRequestHandler(cvPixelBuffer: image, orientation: .leftMirrored, options: [:])
+    let imageResultHandler = VNImageRequestHandler(
+      cvPixelBuffer: image, orientation: .leftMirrored, options: [:])
     try? imageResultHandler.perform([faceDetectionRequest])
   }
 
@@ -190,7 +250,8 @@ class RnFaceBlurView: UIView {
     clearDrawings()
 
     let facesBoundingBoxes: [CAShapeLayer] = observedFaces.map { observedFace in
-      let faceBoundingBoxOnScreen = previewLayer.layerRectConverted(fromMetadataOutputRect: observedFace.boundingBox)
+      let faceBoundingBoxOnScreen = previewLayer.layerRectConverted(
+        fromMetadataOutputRect: observedFace.boundingBox)
       let faceBoundingBoxPath = CGPath(rect: faceBoundingBoxOnScreen, transform: nil)
       let faceBoundingBoxShape = CAShapeLayer()
 
@@ -214,16 +275,103 @@ class RnFaceBlurView: UIView {
 }
 
 extension RnFaceBlurView: AVCaptureVideoDataOutputSampleBufferDelegate {
-  func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+  func captureOutput(
+    _ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer,
+    from connection: AVCaptureConnection
+  ) {
     guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
       return
     }
+
     detectFace(image: pixelBuffer)
+
+    if isRecording {
+      processVideoFrame(sampleBuffer: sampleBuffer)
+    }
+  }
+
+  private func processVideoFrame(sampleBuffer: CMSampleBuffer) {
+    guard let assetWriter = assetWriter,
+      let assetWriterInput = assetWriterInput,
+      assetWriterInput.isReadyForMoreMediaData
+    else {
+      return
+    }
+
+    let presentationTime = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
+
+    if startTime == nil {
+      startTime = presentationTime
+      assetWriter.startSession(atSourceTime: presentationTime)
+    }
+
+    // Create an image context and draw the overlay
+    UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, 0.0)
+    let context = UIGraphicsGetCurrentContext()!
+
+    // Draw the video frame
+    let ciImage = CIImage(cvPixelBuffer: CMSampleBufferGetImageBuffer(sampleBuffer)!)
+    let cgImage = CIContext().createCGImage(ciImage, from: ciImage.extent)!
+    context.draw(cgImage, in: self.bounds)
+
+    // Draw the face detection overlay
+    for drawing in drawings {
+      drawing.render(in: context)
+    }
+
+    let overlayImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    if let cgImage = overlayImage?.cgImage,
+      let pixelBuffer = pixelBufferFromCGImage(cgImage)
+    {
+      pixelBufferAdaptor?.append(pixelBuffer, withPresentationTime: presentationTime)
+    }
+  }
+
+  private func pixelBufferFromCGImage(_ image: CGImage) -> CVPixelBuffer? {
+    let width = image.width
+    let height = image.height
+
+    let attributes: [String: Any] = [
+      kCVPixelBufferCGImageCompatibilityKey as String: kCFBooleanTrue!,
+      kCVPixelBufferCGBitmapContextCompatibilityKey as String: kCFBooleanTrue!,
+    ]
+
+    var pixelBuffer: CVPixelBuffer?
+    let status = CVPixelBufferCreate(
+      kCFAllocatorDefault, width, height, kCVPixelFormatType_32ARGB, attributes as CFDictionary,
+      &pixelBuffer)
+
+    guard status == kCVReturnSuccess, let pixelBuffer = pixelBuffer else {
+      return nil
+    }
+
+    CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+    let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
+
+    let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+    let context = CGContext(
+      data: pixelData,
+      width: width,
+      height: height,
+      bitsPerComponent: 8,
+      bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer),
+      space: rgbColorSpace,
+      bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+    context?.draw(image, in: CGRect(x: 0, y: 0, width: width, height: height))
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+
+    return pixelBuffer
   }
 }
 
 extension RnFaceBlurView: AVCaptureFileOutputRecordingDelegate {
-  func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+  func fileOutput(
+    _ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL,
+    from connections: [AVCaptureConnection], error: Error?
+  ) {
     if error == nil {
       saveVideoToPhotos(outputFileURL: outputFileURL)
     } else {
